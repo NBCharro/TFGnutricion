dfaa120321407853bdf9d1570fb12339c6dd3de3
Nelson Blanco Charro <nbcharro@gmail.com>
2023-04-04 13:27:56 +0200

    conexion con api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,36 @@
 @section('page-title', 'Pagina de pruebas')
 @section('content-area')
     <section class="relative dark:text-white text-dark">
-        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-            <form action="#" class="space-y-8">
-                <div class="sm:col-span-2">
-                    <label for="message" class="block mb-2 text-sm">Mensaje</label>
-                    <textarea id="message" rows="6"
-                        class="bg-gray-50 border text-sm rounded-lg
-                         w-full p-2.5 resize-none"
-                        placeholder="Escriba su mensaje"></textarea>
-                </div>
-                <button type="submit"
-                    class="py-3 px-5 text-sm text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar</button>
-            </form>
-        </div>
+        <button id="botonAPI"
+            class="py-3 px-5 text-sm text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar</button>
     </section>
+    <script>
+        const botonAPI = document.getElementById('botonAPI');
+        botonAPI.addEventListener('click', (event) => {
+            event.preventDefault;
+            pruebaAPI(comida[0]);
+
+        })
+        const comida = [
+            'higado cerdo',
+            'morcilla de embutido',
+            'golden grahams',
+            'fabada asturiana litoral',
+            'callos a la madrileña la tila',
+            'lentejas',
+            'lentejas con chorizo litoral',
+            'higado ternera',
+            'pastel manzana hojaldre',
+            'galletas choco flakes',
+            'donettes',
+            'lomo embuchado'
+        ];
+        async function pruebaAPI(comida) {
+            // let respuesta = await fetch(`https://api-nutricion.onrender.com/api/v1/alimentos/nombre=${comida}`)
+            let respuesta = await fetch(`http://localhost:3000/api/v1/alimentos/nombre=${comida}`)
+            // let respuesta = await fetch(`http://localhost:3000/api/v1/alimentos`)
+            let json = await respuesta.json();
+            console.log(json);
+        }
+    </script>
 @endsection
